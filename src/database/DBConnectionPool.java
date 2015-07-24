656138776fcd113e8a30563efcc218de8d3be647
steffieblack <steffie.black78@gmail.com>
2015-07-24 08:29:38 +0200

    Merge origin/master
    Conflicts:
    	src/gui/MainWindow.java

--- src/database/DBConnectionPool.java
+++ src/database/DBConnectionPool.java
@@ -5,8 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.LinkedList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class DBConnectionPool {
